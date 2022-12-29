b3677fb93698b2bf8695cb8a8816b8f70591b9df
jajablinky <gmillar6@gmail.com>
2022-12-28 17:34:59 -0800

    header and first section-started with bg gradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import { useState } from "react";
-import { Router } from "react-router-dom";
+import { useState } from 'react'
+import { Router } from 'react-router-dom'
 
-import "./App.css";
-import Nav from "./components/Nav";
+import './styles/index.css'
+import './styles/App.css'
+import Nav from './components/Nav'
 
 function App() {
-  return (
-    <div className="full--container">
-      <Nav />
-      <main>
+    return (
+        <div className="full--container">
+            <div className="content--container">
+                <main>
+                    <Nav />
+                    <section className="landing-page--container">
+                        <div></div>
+                    </section>
+                </main>
+            </div>
+            {/* <main>
         <section></section>
         <section></section>
-        <section></section>
-      </main>
-    </div>
-  );
+      </main> */}
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,38 @@
-import React from "react";
-import { HashLink } from "react-router-hash-link";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { HashLink } from 'react-router-hash-link'
+
+import foobar from '../images/foobar.png'
 
 const Nav = () => {
-  return (
-    <nav>
-      <HashLink smooth to="/#home">
-        About
-      </HashLink>
+    return (
+        <header className="header--container">
+            <div className="header--container--layer2"></div>
+            <div className="header--sub-container">
+                <div className="header--content-container">
+                    <div className="header--content-left">
+                        <img
+                            className={'foobar'}
+                            src={foobar}
+                            alt={'foobar_transparent'}
+                        />
+                        <h1>Foobar</h1>
+                    </div>
+                    <nav>
+                        <HashLink smooth to="/#blog" className="menu-item">
+                            Blog
+                        </HashLink>
+                        <HashLink smooth to="/#audits" className="menu-item">
+                            Audits
+                        </HashLink>
+                        <Link>
+                            <button className="contact--button">Contact</button>
+                        </Link>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
 
-      <HashLink smooth to="/#services">
-        Profile
-      </HashLink>
-
-      <HashLink smooth to="/#testimonial">
-        Services
-      </HashLink>
-    </nav>
-  );
-};
-
-export default Nav;
+export default Nav
