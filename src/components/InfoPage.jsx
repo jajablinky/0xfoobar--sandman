0f0d2ae7b88ba2e2c519cc7a74d8ab91582d0952
jajablinky <gmillar6@gmail.com>
2022-12-29 22:28:56 -0800

    help your engineers section with three boxes is complete

---
+++ src/components/InfoPage.jsx
@@ -0,0 +1,50 @@
+const InfoPage = () => {
+    return (
+        <>
+            <section class="info-page--main-container">
+                <div className="help-engineers--header">
+                    <h4>Help Your Engineers</h4>
+                    <p className="info-box--p">
+                        Keep your contracts safe with Foobar. Your developers
+                        will thank you!
+                    </p>
+                    <div className="info-box--container">
+                        <div className="info-box--1">
+                            <h5 className="info-box--header">
+                                Coding Mistakes
+                            </h5>
+                            <p className="info-box--p">
+                                Avoid coding mistakes that could cause problems
+                                like not checking input carefully, and
+                                forgetting to set up data.
+                            </p>
+                        </div>
+                        <div className="info-box--2">
+                            <h5 className="info-box--header">Design Flaws</h5>
+                            <p className="info-box--p">
+                                Find problems in the design of the important
+                                parts of your business. Make sure it's what you
+                                really want, even if the code seems right
+                            </p>
+                        </div>
+                        <div className="info-box--3">
+                            <h5 className="info-box--header">Money Safety</h5>
+                            <p className="info-box--p">
+                                Expose un-safe flaws, including price
+                                manipulation, flash loan hazards, MEV,
+                                front-running, and more.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="info-box--container">
+                        <div className="info-box--1-secondlayer"></div>
+                        <div className="info-box--2-secondlayer"></div>
+                        <div className="info-box--3-secondlayer"></div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default InfoPage
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+/* Image Import */
+import contractStacks from '../images/Contract-Stacks.png'
+import alchemix from '../images/client-logos/Alchemix.png'
+import alpacadabraz from '../images/client-logos/alpacadabraz.png'
+import delegateCash from '../images/client-logos/Delegate-cash.png'
+import fooFighters from '../images/client-logos/Foofighters.png'
+import oSnipeGenesis from '../images/client-logos/oSnipeGenesis.png'
+import partyBid from '../images/client-logos/Partybid_logo.png'
+import pixelLab from '../images/client-logos/Pixellab.png'
+import substack from '../images/client-logos/Substack.png'
+import sushiSwap from '../images/client-logos/sushiSwap.png'
+import tallyLogo from '../images/client-logos/TallyLogo 1.png'
+
+const LandingPage = () => {
+    return (
+        <>
+            <section className="landing-page--main-container">
+                <div className="landing-page--left">
+                    <h1 className="landing-page--left-save-your">save your</h1>
+                    <h1 className="landing-page--left-smart-contracts">
+                        smart contracts
+                    </h1>
+                    <span className="landing-page--left-secondary">
+                        EVM smart contract audits on NFT and Defi. Saving you
+                        millions of dollars.
+                    </span>
+                    <button className="landing-page--lets-talk-button">
+                        Lets Talk
+                    </button>
+                </div>
+                <div className="landing-page--right">
+                    <img src={contractStacks} alt="contract-stacks" />
+                </div>
+            </section>
+            <div className="trusted-by--wrapper">
+                <div className="trusted-by--wrapper-secondlayer"></div>
+                <div className="trusted-by--container">
+                    <div className="trusted-by--header">
+                        <h3>Trusted By:</h3>
+                    </div>
+                    <div className="trusted-by--logo-grid">
+                        <img src={alchemix} alt="contract-stacks" />
+                        <img src={alpacadabraz} alt="contract-stacks" />
+                        <img src={delegateCash} alt="contract-stacks" />
+                        <img src={sushiSwap} alt="contract-stacks" />
+                        <img src={fooFighters} alt="contract-stacks" />
+                        <img src={oSnipeGenesis} alt="contract-stacks" />
+                        <img src={substack} alt="contract-stacks" />
+                        <img src={tallyLogo} alt="contract-stacks" />
+                        <img src={partyBid} alt="contract-stacks" />
+                        <img src={pixelLab} alt="contract-stacks" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LandingPage
