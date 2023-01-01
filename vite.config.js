91c138ad0eab3ea9838c7993511e6e459d7cfc4a
jajablinky <gmillar6@gmail.com>
2023-01-01 15:37:15 -0800

    build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: '/vite-app-sample/',
+    plugins: [react()],
 })
