093e8ce9f5c81f3809dc1ae6e75d062d0d00efa1
jajablinky <gmillar6@gmail.com>
2023-02-15 21:30:56 -0800

    added twinkling stars

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,21 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 
 import foostudio from '../images/foostudio_logo.png'
+import Stars from './Stars'
 
 const LandingPage = () => {
 const quote = "Think of us as the sandman - you can sleep at night tucked in as your project is secured"
 
+useEffect(() => {
+    Stars()
+}, [])
 
     return (
         <>
             <section className="landing-page--full-container">
-                <div className='landing-page--content'>
+                <div className='landing-page--content fade-in'>
                         <img
                             className={'foostudio'}
                             src={foostudio}
                             alt={'foostudios'}
                         />
-                        <p className='quote'>{quote}</p>
+                        <p>{quote}</p>
                 </div>
             </section>
         </>
---
+++ src/components/Stars.jsx
@@ -0,0 +1,26 @@
+const Stars = () => {
+    const count = 20; 
+    const location = document.querySelector('.landing-page--full-container');
+    let i = 0;
+
+    while (i < count) {
+        const star = document.createElement('i');
+        const x = Math.floor(Math.random() * window.innerWidth)
+        const y = Math.floor(Math.random() * window.innerHeight)
+
+        const size = Math.random() * 4;
+        star.style.left = x+'px';
+        star.style.top = y+'px';
+        star.style.width = 1+size+'px';
+        star.style.height = 1+size+'px';
+
+        const duration = Math.random() * 4;
+
+        star.style.animationDuration = 2+duration+'s'
+
+        location.appendChild(star);
+        i++;
+    }
+}
+
+export default Stars;
